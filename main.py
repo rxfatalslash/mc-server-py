0b310f67d8596edd3f8d4b73d2ceae7d1de418cb
rxfatalslash <aaronsanchezmenendez@gmail.com>
2023-12-10 23:28:45 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,140 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @author: rxfatalslash
+
+import requests
+import numpy as np
+from tqdm.auto import tqdm
+import re
+import subprocess
+import os
+import sys
+import platform
+
+url = "https://files.minecraftforge.net/net/minecraftforge/forge/promotions_slim.json"
+response = requests.get(url)
+
+if platform.system() == "Linux" and os.geteuid() != 0:
+    print("\033[031m\033[1mERROR:\033[0m Este script se debe ejecutar como usuario root")
+    sys.exit(1)
+
+while True:
+    r = input("¿Quieres instalar mods? [s/N] ")
+    match r.lower():
+        case "s":
+            if len(sys.argv) < 2:
+                os.system('clear')
+                print("\033[31m\033[1mERROR:\033[0m Pasa la carpeta con los mods deseados como argumento")
+                sys.exit(1)
+            else:
+                if os.path.exists(sys.argv[1]):
+                    if os.path.isdir(sys.argv[1]):
+                        break
+                    else:
+                        os.system('clear')
+                        print("\033[31m\033[1mERROR:\033[0m El elemento no es una carpeta")
+                        sys.exit(1)
+                else:
+                    os.system('clear')
+                    print("\033[31m\033[1mERROR:\033[0m El elemento no existe")
+                    sys.exit(1)
+        case "n":
+            break
+        case _:
+            print("\033[31m\033[1mERROR:\033[0m Introduce solo 's' o 'n'")
+
+if response.status_code == 200:
+    data = response.json()
+    promos = data['promos']
+    last_version = np.array(list(promos.items())[-1])
+
+    if promos:
+        while True:
+            os.system('clear')
+            r = input("¿Qué versión quieres? [Recommended/Latest] ")
+            os.system('clear')
+            match r.lower():
+                case "recommended":
+                    break
+                case "latest":
+                    break
+                case _:
+                    print("\033[31m\033[1mERROR:\033[0m Introduce uno de los valores indicados")
+
+        for version, id in promos.items():
+            if r.lower() in version:
+                print(f"\033[32m\033[1mVersion:\033[0m {version}, id: {id}")
+
+        while True:
+            ve = input("\n¿Qué número de versión quieres? [1.1-1.20.4] ")
+            v = ve.split("-")[0]
+            os.system('clear')
+            if re.search("^[1-9]{1}\.[0-9]{1,2}\.[0-9]{1,2}$", v):
+                break
+            else:
+                print("\033[31m\033[1mERROR:\033[0m Introduce uno de los valores indicados")   
+
+        for version, id in promos.items():
+            if v in version and isinstance(r, str) and r.lower() in version:
+                print(f"\033[32m\033[1mVersión:\033[0m {version}\n")
+                
+                url = f"https://maven.minecraftforge.net/net/minecraftforge/forge/{v}-{id}/forge-{v}-{id}-installer.jar"
+                response = requests.get(url, stream=True)
+
+                match platform.system():
+                    case "Linux":
+                        path = "/opt/minecraft"
+                        if not os.path.exists(path):
+                            os.mkdir(path)
+                            with open(f"{path}/forge-{v}-installer.jar", "wb") as r:
+                                with tqdm(
+                                    unit='B', unit_scale=True, unit_divisor=1024, miniters=1,
+                                    desc="\033[32m\033[1mEstado de descarga\033[0m", total=int(response.headers.get('content-length', 0))
+                                ) as pbar:
+                                    for chunk in response.iter_content(chunk_size=4096):
+                                        r.write(chunk)
+                                        pbar.update(len(chunk))
+                    case "Windows":
+                        path = f"C:\{os.environ['USERNAME']}\Documents\minecraft"
+                        if not os.path.exists(path):
+                            os.mkdir(path)
+                            with open(f"{path}/forge-{v}-installer.jar", "wb") as r:
+                                with tqdm(
+                                    unit='B', unit_scale=True, unit_divisor=1024, miniters=1,
+                                    desc="\033[32m\033[1mEstado de descarga\033[0m", total=int(response.headers.get('content-length', 0))
+                                ) as pbar:
+                                    for chunk in response.iter_content(chunk_size=4096):
+                                        r.write(chunk)
+                                        pbar.update(len(chunk))
+                    case _:
+                        print("\033[31m\033[1mERROR:\033[0m Esta plataforma no tiene soporte")
+                        sys.exit(1)
+
+
+        match platform.system():
+            case "Linux":
+                try:
+                    os.system('clear')
+                    subprocess.run(['chmod', '+x', 'setup.sh'])
+                    if len(sys.argv) < 2:
+                        subprocess.run('./setup.sh')
+                    else:
+                        subprocess.run(['./setup.sh', sys.argv[1]])
+                except Exception:
+                    print("\033[31m\033[1mERROR:\033[0m El fichero no existe")
+                    sys.exit(1)
+            case "Windows":
+                try:
+                    os.system('cls')
+                    if len(sys.argv) < 2:
+                        subprocess.run('./setup.bat')
+                    else:
+                        subprocess.run(['./setup.bat', sys.argv[1]])
+                except Exception:
+                    print("\033[31m\033[1mERROR:\033[0m El fichero no existe")
+                    sys.exit(1)
+            case _:
+                print("\033[31m\033[1mERROR:\033[0m Esta plataforma no tiene soporte")
+                sys.exit(1)
+else:
+    print("\033[31m\033[1mERROR\033[0m Código de estado:", response.status_code)
---
+++ setup.sh
@@ -0,0 +1,249 @@
+#!/bin/sh
+# @author: rxfatalslash
+
+# Variables de color
+CRE=$(tput setaf 1)
+CGR=$(tput setaf 2)
+CYE=$(tput setaf 3)
+BLD=$(tput bold)
+CNC=$(tput sgr0)
+
+# Variables
+modpack=$1
+dir="/opt/minecraft"
+
+# Función para versiones antiguas
+old_install() {
+    clear
+    logo "Instalando servidor..."
+    cd $dir
+    java -jar $dir/forge-*-installer.jar --installServer 2>/dev/null # Instalación del servidor
+    rm $dir/forge-*-installer.jar $dir/forge-*-installer.jar.log
+    clear
+    read -rp "[+] ¿Cúanta memoria RAM en GB quieres que tenga tu servidor? [2-8] " ram # RAM máxima del servidor
+    read -rp "[+] ¿Quieres cambiar la dificultad? [s/N] " dif
+    while true; do
+        case $dif in
+            [sS]*)
+                printf '%*s\n\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' _
+                printf "0) %s%sPacífico%s\n\n" "${BLD}" "${CGR}" "${CNC}"
+                printf "1) %s%sFácil%s\n\n" "${BLD}" "${CGR}" "${CNC}"
+                printf "2) %s%sNormal%s\n\n" "${BLD}" "${CGR}" "${CNC}"
+                printf "3) %s%sDifícil%s\n\n" "${BLD}" "${CGR}" "${CNC}"
+                read -rp "[+] ¿Qué dificultad quieres? [0-3] " d
+                while true; do
+                    case $d in
+                        [0123]*)
+                            echo "difficulty=$d\n" > $dir/server.properties
+                            break
+                        ;;
+                        *) printf "%s%sERROR:%s Introduce solo una de las opciones anteriores\n" "${BLD}" "${CRE}" "${CNC}";;
+                    esac
+                done
+                break
+            ;;
+            [nN]*) break;;
+            *) printf "%s%sERROR:%s Introduce solo 's' o 'n'\n" "${BLD}" "${CRE}" "${CNC}";;
+        esac
+    done
+    read -rp "[+] ¿Quieres cambiar el puerto del servidor? [s/N] " port
+    while true; do
+        case $port in
+            [sS]*)
+                read -rp "[+] ¿Qué puerto quieres utilizar? [1025-65535] " p
+                echo "server-port=$p\n" >> $dir/server.properties
+                break
+            ;;
+            [nN]*) break;;
+            *) printf "%s%sERROR:%s Introduce solo 's' o 'n'\n" "${BLD}" "${CRE}" "${CNC}";;
+        esac
+    done
+    clear
+    logo "Configurando servidor..."
+    sleep 3
+    java -Xmx"$ram"G -jar $dir/minecraft_server.*.jar nogui
+    echo "eula=true" > $dir/eula.txt # Aceptar EULA
+    if [ $modpack ]; then
+        if [ ! -d $dir/mods ]; then
+            mkdir -p $dir/mods
+        fi
+        cp -r $modpack/* $dir/mods
+    fi
+    read -rp "[+] ¿Quieres poner una semilla personalizada? [s/N] " seed
+    while true; do
+        case $seed in
+            [sS]*)
+                read -rp "[+] Introduce la semilla: " s
+                echo "level-seed=$s\n" >> $dir/server.properties
+                break
+            ;;
+            [nN]*) break;;
+            *) printf "%s%sERROR:%s Introduce solo 's' o 'n'\n" "${BLD}" "${CRE}" "${CNC}";;
+        esac
+    done
+    logo "Iniciando servidor..."
+    java -Xmx"$ram"G -jar $dir/minecraft_server*.jar nogui
+}
+
+# Función para versiones nuevas
+new_install() {
+    clear
+    logo "Instalando servidor..."
+    cd $dir
+    java -jar $dir/forge-*-installer.jar --installServer 2>/dev/null # Instalación del servidor
+    rm $dir/forge-*-installer.jar $dir/forge-*-installer.jar.log
+    clear
+    read -rp "[+] ¿Cúanta memoria RAM en GB quieres que tenga tu servidor? [2-8] " ram
+    read -rp "[+] ¿Quieres cambiar la dificultad? [s/N] " dif
+    while true; do
+        case $dif in
+            [sS]*)
+                printf '%*s\n\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' _
+                printf "0) %s%sPacífico%s\n\n" "${BLD}" "${CGR}" "${CNC}"
+                printf "1) %s%sFácil%s\n\n" "${BLD}" "${CGR}" "${CNC}"
+                printf "2) %s%sNormal%s\n\n" "${BLD}" "${CGR}" "${CNC}"
+                printf "3) %s%sDifícil%s\n\n" "${BLD}" "${CGR}" "${CNC}"
+                read -rp "[+] ¿Qué dificultad quieres? [0-3] " d
+                echo "difficulty=$d\n" > $dir/server.properties
+                break
+            ;;
+            [nN]*) break;;
+            *) printf "%s%sERROR:%s Introduce solo 's' o 'n'\n" "${BLD}" "${CRE}" "${CNC}";;
+        esac
+    done
+    read -rp "[+] ¿Quieres cambiar el puerto del servidor? [s/N] " port
+    while true; do
+        case $port in
+            [sS]*)
+                read -rp "[+] ¿Qué puerto quieres utilizar? [1025-65535] " p
+                echo "server-port=$p\n" >> $dir/server.properties
+                break
+            ;;
+            [nN]*) break;;
+            *) printf "%s%sERROR:%s Introduce solo 's' o 'n'\n" "${BLD}" "${CRE}" "${CNC}";;
+        esac
+    done
+    clear
+    logo "Configurando servidor..."
+    echo "-Xmx"$ram"G" > $dir/user_jvm_args.txt # RAM máxima del servidor
+    echo "eula=true" > $dir/eula.txt # Aceptar EULA
+    if [ $modpack ]; then
+        if [ ! -d $dir/mods ]; then
+            mkdir -p $dir/mods
+        fi
+        cp -r $modpack/* $dir/mods
+    fi
+    read -rp "[+] ¿Quieres poner una semilla personalizada? [s/N] " seed
+    while true; do
+        clear
+        case $seed in
+            [sS]*)
+                read -rp "[+] Introduce la semilla: " s
+                echo "level-seed=$s" >> $dir/server.properties
+                break
+            ;;
+            [nN]*) break;;
+            *) printf "%s%sERROR:%s Introduce solo 's' o 'n'\n" "${BLD}" "${CRE}" "${CNC}";;
+        esac
+    done
+    clear
+    logo "Iniciando servidor..."
+    sh $dir/run.sh
+}
+
+# Logo
+logo () {
+
+    local text="${1:?}"
+    echo -en "
+    WOdddddxk0KNW                                            WNK0kxdddddOW 
+     Wo........',:lx0N                                    N0xl:,'........oW 
+      Xc.,clcc:;'....;lON                              NOl;....';:cclc,.cX  
+       Kc'cdodlc::ccc;..:kN                          Nk:..;ccc::cldodc'cK   
+        Xo;:loooooooool:,'cK                        Kc',:loooooooool:;oX    
+         WKxolccclllllllc;';OW                    WO;';clllllllcccloxKW     
+            WNKOOOOkkkOkkkdokN                    NkodkkkOkkkOOOOKNW       
+    \n\n"
+    printf ' %s [%s%s %s%s %s]%s\n\n' "${CRE}" "${CNC}" "${CYE}" "${text}" "${CNC}" "${CRE}" "${CNC}"
+}
+
+logo "rxfatalslash"
+
+# Root
+if [ $EUID -ne 0 ]; then
+    printf "%s%sERROR:%s Ejecuta el script como root\n" "${BLD}" "${CRE}" "${CNC}"
+    exit 1
+fi
+
+printf "%s%sEste script automatiza la instalación de un servidor de Minecraft con mods%s\n\n" "${BLD}" "${CGR}" "${CNC}"
+
+# Confirmación
+while true; do
+    read -rp "[+] ¿Quieres continuar? [s/N] " confirm
+    clear
+    case $confirm in
+        [sS]*) break;;
+        [nN]*) exit 1;;
+        *) printf "%s%sError:%s Escribe solo 's' o 'n'\n\n" "${BLD}" "${CRE}" "${CNC}";;
+    esac
+done
+clear
+
+# Distro
+distro="$(cat /etc/os-release | cut -d '=' -f2 | sed -e 's/"//g' | awk 'NR==1 {print $1}')"
+case "$distro" in
+    ["Arch Linux""Arch"]*)
+        printf "%s%sSistema Arch Linux detectado%s\n\n" "${BLD}" "${CGR}" "${CNC}"
+        jdk="$(pacman -Qs jdk*-openjdk)"
+        wget="$(pacman -Qs wget)"
+        mkdir -p $dir
+
+        # JDK check
+        if [[ -z $jdk ]]; then
+            pacman -S jdk-openjdk --noconfirm
+        fi
+
+        # Wget check
+        if [[ -z $wget ]]; then
+            pacman -S wget --noconfirm
+        fi
+
+        # Comprobar versión
+        file="$(basename $dir/forge-*-installer.jar | cut -d '-' -f2 | cut -d '.' -f2)"
+
+        if [ $file -le 16 ]; then
+            old_install
+        else
+            new_install
+        fi
+    ;;
+    ["Ubuntu""Debian"]*)
+        printf "%s%sSistema Ubuntu detectado%s\n\n" "${BLD}" "${CGR}" "${CNC}"
+        jdk="$(apt-cache search openjdk-*-jdk)"
+        wget="$(apt-cache search wget)"
+        mkdir -p $dir
+        
+        # JDK check
+        if [[ -z $jdk ]]; then
+            apt-get install -y openjdk-17-jdk
+        fi
+
+        # Wget check
+        if [[ -z $wget ]]; then
+            apt-get install -y wget
+        fi
+
+        # Comprobar versión
+        file="$(basename $dir/forge-*-installer.jar | cut -d '-' -f2 | cut -d '.' -f2)"
+
+        if [ $file <= 16 ]; then
+            old_install
+        else
+            new_install
+        fi
+    ;;
+    *)
+        printf "%s%sERROR:%s No existe soporte para esta versión de Linux\n\n" "${BLD}" "${CRE}" "${CNC}"
+        exit 1
+    ;;
+esac
